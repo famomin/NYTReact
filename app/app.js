72ba544fd9e06ef1893fc0b0d46da060ee65ed23
Faras Momin <famomin@utexas.edu>
2017-03-19 01:37:51 -0500

    initial app.js, has main render to id app

---
+++ app/app.js
@@ -0,0 +1,9 @@
+//Main React Dependencies
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+// Require Main Component
+var Main = require("./components/Main");
+
+//Main Component
+ReactDOM.render(<Main />, document.getElementById("app"));
